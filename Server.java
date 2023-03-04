fe1ba184554ce364de5bc418261874b377908e93
GW18451 <125784911+GW18451@users.noreply.github.com>
2023-03-04 13:23:41 +0000

    Pending changes exported from your codespace

---
+++ Server.java
@@ -0,0 +1,20 @@
+pacakge atm.simulator.system;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import.java.sql.*;
+
+public class Logln extends JFrame implements ActionListener {
+
+JTextField tf1;
+JPasswordField pf2;
+JButton b1, b2, b3;
+
+Login()(
+    setTitle=("Automated Teller Machine");
+
+    ImageIcon 11 = new ImageIcon(ClassLoader,getSystemResource("Icons/logo.jpg"))
+    Image 12 = 11.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+    ImageIcon 13 = new ImageaIcon (12);
+    JLabel 111 = mew JLabel(13);
